34657ded789ca7b33ddc85d70f2923d164f4b88c
Leonardo Telles <leo.c.telles@gmail.com>
2020-07-28 18:48:50 -0300

    Add PageDefault

--- src/pages/Cadastro/Video/index.js
+++ src/pages/Cadastro/Video/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import PageDefault from "../../../components/PageDefault";
+
 function CadastroVideo() {
-  return <h1>Cadastro de Vídeo</h1>;
+  return (
+    <PageDefault>
+      <div style={{ margin: "100px" }}>
+        <h1>Cadastro de Vídeo</h1>
+      </div>
+    </PageDefault>
+  );
 }
 
 export default CadastroVideo;
